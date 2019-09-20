f9343372c93aa36c2cd013f9057aed7a0c5df203
Alexander <ale097@csiro.au>
2019-09-20 11:31:08 +1000

    After morning tea

--- Scripts/R scripts.R
+++ Scripts/R scripts.R
@@ -6,5 +6,33 @@ select(gapminder, country, year, pop)
 gapminderfiltered <- select(gapminder, country, year, pop)
 filter(gapminderfiltered, country == "Australia")
 australia <- filter(gapminderfiltered, country == "Australia")
+
+#select will return columns
 select(filter(gapminder, country =="Australia"), country, year, pop)
+
+
+#filter will return rows
 filter(select(gapminder, country, year, pop), country == "Australia")
+
+
+select(gapminder, year, pop)
+
+#piping is a way of funneling down 
+gapminder %>% select(year, pop)
+
+gapminder %>% filter(country == "Australia") %>% 
+  select(country,year, pop)
+
+filter(gapminder, country == "Australia", year >=1997)
+#or with piping
+small <- gapminder %>% filter(country=="Australia", year >=1997)
+small
+
+gapminder %>% 
+  filter(country == "Australia") %>% 
+  select(country, year, pop)
+#this is much easier to read
+
+Australian_Year_Pop <- gapminder %>% 
+  filter(country == "Australia") %>% 
+  select(country, year, pop)
